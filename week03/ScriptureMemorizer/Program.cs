ee38bbe517a19305032e5d768c3d39fed0bdc247
Mufaro <mufaromaz@gmail.com>
2025-07-19 15:06:22 +0200

    final draft

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,17 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Welcome message
+        
         Console.WriteLine("Welcome to the Scripture Memorizer!");
         Console.WriteLine("Press Enter to begin...");
         Console.ReadLine();
 
-        // Create a reference and a scripture
         Reference reference = new Reference("Proverbs", 3, 5, 6);
         string scriptureText = "Trust in the Lord with all thine heart; and lean not unto thine own understanding.";
         Scripture scripture = new Scripture(reference, scriptureText);
 
-        // Loop until all words are hidden or user types "quit"
         while (true)
         {
            // Console.Clear();
