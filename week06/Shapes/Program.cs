06c602badcd05bfcbfce280ddcee57cb255d01b6
Mufaro <mufaromaz@gmail.com>
2025-08-06 23:06:01 +0200

    shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create instances
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Circle circle = new Circle("Green", 2.5);
+
+        // Test individual shapes
+        Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea()}");
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+        Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        // List of shapes
+        List<Shape> shapes = new List<Shape>() { square, rectangle, circle };
+
+        Console.WriteLine("\nIterating through shape list:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
